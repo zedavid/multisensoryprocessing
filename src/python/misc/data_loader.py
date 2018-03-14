c214f6eaf440e85da91f2785dc2cd6d8f9ec40c4
jdlopes <jdlopes@kth.se>
2018-03-14 14:08:55 +0100

    Data loader to check what is available in each folder

--- src/python/misc/data_loader.py
+++ src/python/misc/data_loader.py
@@ -25,25 +25,80 @@ def load_video_raw_data(root_dir, filename):
     height_cv = int(file_info['img_size']['height'])
     width_cv = int(file_info['img_size']['width'])
 
-    with open(os.path.join(root_dir,filename), 'rb') as f:
-        unpacker = msgpack.Unpacker(f)
-        for value, timestamp in unpacker:
-            # waveFile.writeframes(value)
-            print(np.frombuffer(value, dtype='uint8').shape)
-            cv2.imshow('data', np.frombuffer(value, dtype='uint8').reshape((height_cv, width_cv, 3)))
-            cv2.waitKey(1)
+    try:
+        with open(os.path.join(root_dir,filename), 'rb') as f:
+            unpacker = msgpack.Unpacker(f)
+            for value, timestamp in unpacker:
+                cv2.imshow('data', np.frombuffer(value, dtype='uint8').reshape((height_cv, width_cv, 3)))
+                cv2.waitKey(1)
+    except KeyboardInterrupt:
+        cv2.destroyAllWindows()
+        return
 
 
 def load_video_data(filename):
     cap = cv2.VideoCapture(filename)
 
-    while(cap.isOpened):
-        ret, frame = cap.read()
-        if ret == True:
-            cv2.imshow('frame', frame.reshape((480,640,3)))
-            cv2.waitKey(1)
-        else:
-            break
+    try:
+        while(cap.isOpened):
+            ret, frame = cap.read()
+            if ret == True:
+                cv2.imshow('frame', frame.reshape((480,640,3)))
+                cv2.waitKey(1)
+            else:
+                break
+    except KeyboardInterrupt:
+        cap.release()
+        cv2.destroyAllWindows()
+        return
+
+def load_audio_raw_data(root_dir,filename):
+    # open stream
+    stream = p.open(format=FORMAT,
+                    channels=1,
+                    rate=RATE,
+                    output=True)
+
+    try:
+        with open(os.path.join(root_dir,filename),'rb') as f:
+            unpacker = msgpack.Unpacker(f)
+            for value, timestamp in unpacker:
+                stream.write(value)
+    except KeyboardInterrupt:
+        stream.close()
+        return
+
+def load_audio_data(filename):
+
+    # define stream chunk
+    chunk = 1024
+
+    # open a wav format music
+    f = wave.open(filename, "rb")
+    # instantiate PyAudio
+    p = pyaudio.PyAudio()
+    # open stream
+    stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
+                    channels=f.getnchannels(),
+                    rate=f.getframerate(),
+                    output=True)
+    # read data
+    data = f.readframes(chunk)
+
+    try:
+        # play stream
+        while data:
+            stream.write(data)
+            data = f.readframes(chunk)
+
+    except KeyboardInterrupt:
+        # stop stream
+        stream.stop_stream()
+        stream.close()
+
+        # close PyAudio
+        p.terminate()
+        return
 
 parser = argparse.ArgumentParser(description='Files in folder')
 parser.add_argument('--folder', '-f', type=str,help='folder where the data is stored',required=True)
@@ -51,9 +106,13 @@ args = parser.parse_args()
 
 for root,dir,files in os.walk(args.folder):
     for file in files:
-        print(file)
-        #if file.endswith('cv-video'):
-        #    load_video_raw_data(root,file)
+        print(os.path.join(root,file))
+        if file.endswith('cv-video'):
+            load_video_raw_data(root,file)
         if file.endswith('mp4'):
             load_video_data(os.path.join(root,file))
+        if file.endswith('data.audio'):
+            load_audio_raw_data(root,file)
+        if file.endswith('wav'):
+            load_audio_data(os.path.join(root,file))
 
